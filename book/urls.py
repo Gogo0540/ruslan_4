fe109143106f15fb7fab5d2b9d277f6aaa86e400
Gogo0540 <ruslan.aslanov2555@gmail.com>
2022-12-20 15:07:50 +0600

    'django_hw_2'

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from book.views import list_book_view
+from book.views import list_book_view, detail_book_view
 
 urlpatterns = [
-    path('books/', list_book_view)
+    path('books/', list_book_view),
+    path('books/<int:id>/', detail_book_view)
 ]
--- book/views.py
+++ book/views.py
@@ -5,3 +5,8 @@ from book.models import Book
 def list_book_view(request):
     book = Book.objects.all()
     return render(request, 'book.html', context={'book': book})
+
+def detail_book_view(request, **kwargs):
+    if request.method == 'GET':
+        book = Book.objects.get(id=kwargs['id'])
+        return render(request, 'detail_book.html',context={'book': book})
