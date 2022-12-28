c2de75c17b17e2959d0cee42199cc15e5cfe45a1
Gogo0540 <ruslan.aslanov2555@gmail.com>
2022-12-28 15:59:13 +0600

    'django_hw'

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from book.views import list_book_view, detail_book_view
+from book.views import list_book_view, detail_book_view, book_create_view, update_book_view, delete_book_view
 
 urlpatterns = [
     path('books/', list_book_view),
-    path('books/<int:id>/', detail_book_view)
+    path('books/<int:id>/', detail_book_view),
+    path("create/book/", book_create_view),
+    path('book/<int:id>/update/', update_book_view),
+    path('book/<int:id>/delete/', delete_book_view),
 ]
--- book/views.py
+++ book/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from book.models import Book
+from .forms import BookForm
+from . import models, forms
 
 
 def list_book_view(request):
-    book = Book.objects.all()
-    return render(request, 'book.html', context={'book': book})
+    if request.method == "GET":
+        book = Book.objects.all()
+        return render(request, 'book.html', context={'book': book})
+
 
 def detail_book_view(request, **kwargs):
     if request.method == 'GET':
         book = Book.objects.get(id=kwargs['id'])
-        return render(request, 'detail_book.html',context={'book': book})
+        return render(request, 'detail_book.html', context={'book': book})
+
+
+def book_create_view(request):
+    if request.method == 'GET':
+        data = {
+            'form': BookForm
+        }
+        return render(request, 'book_create.html', context=data)
+    elif request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        form.save()
+
+        return HttpResponse('книга успешно добавлена')
+
+
+def update_book_view(request, id):
+    show_object = get_object_or_404(models.Book, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(instance=show_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h1>Книга успешно обновлена</h1>')
+    else:
+        form = forms.BookForm(instance=show_object)
+    return render(request, 'book_update.html', {'form': form, 'object': show_object})
+
+def delete_book_view(request, id):
+    show_object = get_object_or_404(models.Book, id=id)
+    show_object.delete()
+    return HttpResponse('<h1>Книга успешна удалена</h1>')
